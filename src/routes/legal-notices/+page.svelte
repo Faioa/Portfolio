d1a5b6be4a0cc9c6575200132c504a9e053fd282
Faioa <.>
2025-11-11 17:42:04 +0100

    Delaying legal notices page

--- src/routes/legal-notices/+page.svelte
+++ src/routes/legal-notices/+page.svelte
@@ -3,3 +3,6 @@
 </script>
 
 <WorkInProgress />
+
+<p>This page is still a little confusing for me so I will work on it later when I finally figure out what I should write.</p>
+<p>Thank you for your understanding.</p>
