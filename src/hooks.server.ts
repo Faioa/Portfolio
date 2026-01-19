347ce10fe1556008c715ed8f7cbe60907e26e6be
Faioa <.>
2026-01-19 12:06:22 +0100

    Saving chosen language in a cookie
    Default language based on the HTTP header *accept-language* still **WIP**

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,9 +1,10 @@
-import { error } from '@sveltejs/kit';
+import { error, redirect } from '@sveltejs/kit';
+import parser from 'accept-language-parser';
 import { loadLocales, runWithLocale } from 'wuchale/load-utils/server';
 
 import { defaultLocale } from '$lib/lang';
+import { locales } from '$lib/lang';
 
-import { locales } from './locales/data.js';
 import * as js from './locales/js.loader.server.js';
 import * as main from './locales/main.loader.server.svelte.js';
 
@@ -12,7 +13,28 @@ loadLocales(js.key, js.loadIDs, js.loadCatalog, locales);
 
 /** @type {import('@sveltejs/kit').Handle} */
 export const handle = async ({ event, resolve }) => {
-	const locale = event.params.locale ?? defaultLocale;
+	/* Getting wanted language for the request, or redirect to client's default language if not specified */
+	const routeLocale = event.params.locale ?? defaultLocale;
+	let locale: string | null | undefined = event.cookies.get('locale');
+
+	// First visit
+	if (!locale) {
+		locale = parser.pick(locales, event.request.headers.get('accept-language') ?? '');
+		if (!locale) locale = defaultLocale;
+		event.cookies.set('locale', locale, { path: '/', maxAge: 60 * 60 * 24 * 7, secure: true }); // maxAge = 7 days
+		// throw redirect
+	}
+
+	// Not first visit + locale change
+	if (routeLocale !== locale) {
+		locale = routeLocale;
+		event.cookies.set('locale', locale, { path: '/', maxAge: 60 * 60 * 24 * 7, secure: true }); // maxAge = 7 days
+	}
+
 	if (!locales.includes(locale)) return error(404, `Unknown content`);
-	return await runWithLocale(locale, () => resolve(event));
+	return await runWithLocale(locale, () =>
+		resolve(event, {
+			transformPageChunk: ({ html }) => html.replace('%lang%', locale)
+		})
+	);
 };
