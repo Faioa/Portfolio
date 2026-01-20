bc2dea13075d6eea17317e326b43686e253109d2
Faioa <.>
2026-01-20 12:50:17 +0100

    Fixing CVs not accessible

--- src/routes/[[locale]]/about/+page.server.ts
+++ src/routes/[[locale]]/about/+page.server.ts
@@ -2,17 +2,13 @@ import { defaultLocale } from '$lib/lang';
 
 import type { PageServerLoad } from './$types';
 
-const available = import.meta.glob('$lib/assets/cv/*', { eager: true });
+const available = import.meta.glob('$lib/assets/cv/*.pdf', {
+	eager: true,
+	query: '?url',
+	import: 'default'
+});
 
 export const load: PageServerLoad = async ({ params }) => {
-	let lang = params.locale ?? defaultLocale;
-
-	if (!(`/src/lib/assets/cv/${lang}.pdf` in available)) {
-		if (lang === defaultLocale || !(`/src/lib/assets/cv/${defaultLocale}.pdf` in available)) {
-			return { cv: null };
-		}
-		lang = defaultLocale;
-	}
-
-	return { cv: `/src/lib/assets/cv/${lang}.pdf` };
+	const lang = params.locale ?? defaultLocale;
+	return { cv: available[`/src/lib/assets/cv/${lang}.pdf`] ?? null };
 };
