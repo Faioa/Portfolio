4531f30ec46c52b337c3cbf76e0fcad4646c0fa2
Faioa <.>
2026-05-11 09:24:51 +0200

    Fixing font-size issue in error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -30,14 +30,16 @@
 
 <div class="flex flex-col items-center justify-center gap-10 text-center">
 	<div class="flex flex-col items-center justify-center gap-2">
-		<span class="text-5xl font-bold">{page.status}</span>
-		<span class="text-3xl">{messages[page.status]?.error}</span>
+		<span class="!text-4xl md:!text-5xl font-bold">{page.status}</span>
+		<span class="!text-2xl md:!text-3xl">{messages[page.status]?.error}</span>
 	</div>
-	<span class="text-lg">{messages[page.status]?.message}</span>
-	<div class="flex justify-center items-center gap-10">
-		<Link href='/' class="bg-secondary py-2 px-5 rounded-2xl text-lg shadow shadow-primary hover:opacity-70">Home</Link>
+	<span class="!text-lg">{messages[page.status]?.message}</span>
+	<div class="flex items-center justify-center gap-10">
+		<Link href="/" class="rounded-2xl bg-secondary px-5 py-2 !text-lg shadow shadow-primary hover:opacity-70">Home</Link>
 		{#if page.route.id?.startsWith('/[[locale=lang]]/articles/[article]')}
-			<Link href='/articles' class="bg-secondary py-2 px-5 rounded-2xl text-lg shadow shadow-primary hover:opacity-70">Articles</Link>
+			<Link href="/articles" class="rounded-2xl bg-secondary px-5 py-2 !text-lg shadow shadow-primary hover:opacity-70"
+				>Articles</Link
+			>
 		{/if}
 	</div>
 </div>
