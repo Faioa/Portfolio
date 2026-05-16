d49cb4018d0197678b5f45ab25456b1a82f81afc
Faioa <.>
2026-05-16 18:14:37 +0200

    Auto Formatting

--- src/lib/components/ArticlesList.svelte
+++ src/lib/components/ArticlesList.svelte
@@ -5,10 +5,12 @@
 	const { articles = $bindable(), metadata = $bindable() } = $props<{ articles: string[]; metadata: Metadata[] }>();
 </script>
 
-<div class="grid flex-wrap truncate grid-cols-1 md:grid-cols-3 w-full gap-10">
+<div class="grid w-full grid-cols-1 flex-wrap gap-10 truncate md:grid-cols-3">
 	{#each articles as article, i (article)}
 		<ArticleTile {article} metadata={metadata[i]} class="col-span-1" />
 	{:else}
-		<p class="col-span-1 md:col-span-3 text-destructive text-center wrap-break-word text-wrap">I'm sorry, but there's no available article yet. Please wait a few days for some content to be published!</p>
+		<p class="col-span-1 md:col-span-3 text-destructive text-center wrap-break-word text-wrap">
+			I'm sorry, but there's no available article yet. Please wait a few days for some content to be published!
+		</p>
 	{/each}
 </div>
