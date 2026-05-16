4512bc5d0e4dc1b64a1ece05acd66262dcb5588c
Faioa <.>
2026-05-16 18:14:16 +0200

    Small fix for overflowing text in the articles' list

--- src/lib/components/ArticlesList.svelte
+++ src/lib/components/ArticlesList.svelte
@@ -9,6 +9,6 @@
 	{#each articles as article, i (article)}
 		<ArticleTile {article} metadata={metadata[i]} class="col-span-1" />
 	{:else}
-		<p class="col-span-1 md:col-span-3 text-destructive text-center">I'm sorry, but there's no available article yet. Please wait a few days for some content to be published!</p>
+		<p class="col-span-1 md:col-span-3 text-destructive text-center wrap-break-word text-wrap">I'm sorry, but there's no available article yet. Please wait a few days for some content to be published!</p>
 	{/each}
 </div>
