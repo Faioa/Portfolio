1da542c17867e3047e0de4a55be7731d7b155ea1
Faioa <.>
2026-05-22 19:16:32 +0200

    Increasing z-index of the header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -18,13 +18,13 @@
 	/* Scroll position in pages during navigation */
 	export const snapshot: Snapshot<number> = {
 		capture: () => window.scrollY,
-		restore: async (previousScrollY) => {
+		restore: async (scrollY) => {
 			await tick();
-			window.scrollTo({ top: previousScrollY, left: 0, behavior: 'instant' });
+			window.scrollTo({ top: scrollY, left: 0, behavior: 'instant' });
 		}
 	};
 
-	/* Necessary to start navigating to a new page at its top instead of the position in the previous page due to SvelteKit behavior */
+	/* Necessary to start navigating to a new page at its top instead of the position in the previous page due to SvelteKit SPA navigation behavior */
 	afterNavigate(({ type }) => {
 		if (type !== 'popstate') window.scrollTo({ top: 0, left: 0, behavior: 'instant' });
 	});
@@ -39,7 +39,7 @@
 <GoTop />
 
 <div class="relative flex min-h-screen w-full flex-col items-center overflow-hidden">
-	<Header class="fixed bg-white/30 backdrop-blur-sm" />
+	<Header class="fixed bg-white/30 backdrop-blur-sm z-100" />
 
 	<main class="relative mt-30 flex w-full grow flex-col items-center justify-center gap-10">
 		{@render children?.()}
